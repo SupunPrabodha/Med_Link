5891e8fa8569283cfd6607dc14dc22c7c52afdcb
Mona Lisa <162087563+DishanGayantha@users.noreply.github.com>
2026-04-15 15:42:30 +0530

    feat: Add telemedicine service with Docker configuration, Maven setup, and initial application structure

---
+++ telemedicine-service/src/test/java/lk/medilink/telemedicine/TelemedicineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.medilink.telemedicine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemedicineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
